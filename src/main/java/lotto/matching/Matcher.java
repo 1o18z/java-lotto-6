e7678a6d86957f0ecf6f6be7a30864e7f616a701
박은지 <qkrdmswl1018@naver.com>
2023-11-19 18:47:49 +0900

    refactor: 결과 매칭 메서드명 및 변수명 수정

--- src/main/java/lotto/matching/Matcher.java
+++ src/main/java/lotto/matching/Matcher.java
@@ -1,23 +1,27 @@
 package lotto.matching;
 
 import java.util.List;
+import lotto.lotto.Bonus;
+import lotto.lotto.LottoList;
+import lotto.lotto.WinningLotto;
 import lotto.result.Grade;
-import lotto.Lotto;
+import lotto.lotto.Lotto;
 import lotto.result.Result;
 
 public class Matcher {
 
-    public Result matchWinning(Lotto winningLotto, List<Lotto> lottoList, int bonus) {
+    public Result match(WinningLotto winningLotto, LottoList lottoList, Bonus bonus) {
         Result result = new Result();
+        List<Lotto> lottos = lottoList.getLottos();
 
-        lottoList.forEach(lotto -> {
+        lottos.forEach(lotto -> {
             long count = lotto.getNumbers().stream()
                     .filter(winningLotto.getNumbers()::contains)
                     .count();
 
-            boolean isBonusMatched = lotto.getNumbers().contains(bonus);
+            boolean hasBonus = lotto.getNumbers().contains(bonus.getNumber());
 
-            Grade grade = Grade.of((int) count, isBonusMatched);
+            Grade grade = Grade.of((int) count, hasBonus);
             result.addResult(grade);
         });
         return result;
