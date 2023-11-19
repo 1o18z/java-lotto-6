b67710f03156ad7c3edaeb5c7f03fbb002556643
박은지 <qkrdmswl1018@naver.com>
2023-11-19 18:59:12 +0900

    refactor: 당첨 로또 검증 로직 작성

--- src/main/java/lotto/lotto/WinningLotto.java
+++ src/main/java/lotto/lotto/WinningLotto.java
@@ -1,15 +1,36 @@
 package lotto.lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import lotto.util.Generator;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static lotto.util.Constant.INVALID_LOTTO_LENGTH;
+import static lotto.util.Constant.LOTTO_LENGTH;
+import static lotto.util.Constant.SEPARATE_LETTER;
 
 public class WinningLotto {
 
     private final List<Integer> numbers;
 
-    public WinningLotto() {
-        this.numbers = Generator.generateLotto();
+    public WinningLotto(String inputNumbers) {
+        this.numbers = new ArrayList<>(validateNumbers(inputNumbers));
+    }
+
+    private List<Integer> validateNumbers(String numbers) {
+        Set<String> set = Arrays.stream(numbers.split(SEPARATE_LETTER)).collect(Collectors.toSet());
+        checkLength(set);
+        return set.stream()
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    private void checkLength(Set<String> set) {
+        if (set.size() != LOTTO_LENGTH) {
+            throw new IllegalArgumentException(INVALID_LOTTO_LENGTH);
+        }
     }
 
     public List<Integer> getNumbers() {
