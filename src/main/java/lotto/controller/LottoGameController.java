313cf194c2d8868948750a8b69c8f426cd480342
박은지 <qkrdmswl1018@naver.com>
2023-11-19 18:40:43 +0900

    refactor: 메인 게임 루프 수정

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -1,27 +1,28 @@
 package lotto.controller;
 
-import java.util.List;
+import lotto.Bonus;
+import lotto.LottoList;
+import lotto.WinningLotto;
 import lotto.result.Calculator;
-import lotto.util.Generator;
 import lotto.input.InputHandler;
-import lotto.Lotto;
 import lotto.matching.Matcher;
 import lotto.result.Result;
 
+import static lotto.util.Constant.LOTTO_PRICE;
+
 public class LottoGameController {
 
-    private final Generator lottoGenerator = new Generator();
     private final Matcher matcher = new Matcher();
     private final InputHandler inputHandler = new InputHandler();
     private final Calculator calculator = new Calculator();
 
     public void run() {
         int purchasedPrice = inputHandler.getPurchasedPrice();
-        List<Lotto> lottoList = lottoGenerator.getLotto(purchasedPrice);
-        Lotto winningLotto = inputHandler.getWinningLotto();
-        int bonus = inputHandler.getBonusNumber(winningLotto);
+        LottoList lottoList = new LottoList(purchasedPrice / LOTTO_PRICE);
+        WinningLotto winningLotto = inputHandler.getWinningLotto();
+        Bonus bonusNumber = inputHandler.getBonusNumber(winningLotto);
 
-        Result result = matcher.matchWinning(winningLotto, lottoList, bonus);
+        Result result = matcher.match(winningLotto, lottoList, bonusNumber);
 
         calculator.getStatistics(result.getMap());
         calculator.getProfitRate(result.getMap(), purchasedPrice);
