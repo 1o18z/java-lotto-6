61d858baf8fea494b2605c4d81f7d8730c2c7b0a
박은지 <qkrdmswl1018@naver.com>
2023-11-19 19:05:13 +0900

    refactor: 예외 메시지를 상수로 분리

--- src/main/java/lotto/util/Constant.java
+++ src/main/java/lotto/util/Constant.java
@@ -22,10 +22,17 @@ public class Constant {
     public static final int MATCHED_FOUR_NUMBER = 4;
     public static final int MATCHED_THREE_NUMBER = 3;
 
-    public static final Long PRICE_FOR_SIX = 2000000000L;
-    public static final Long PRICE_FOR_BONUS = 30000000L;
-    public static final Long PRICE_FOR_FIVE = 1500000L;
-    public static final Long PRICE_FOR_FOUR = 50000L;
-    public static final Long PRICE_FOR_THREE = 5000L;
+    public static final Long PRICE_FOR_SIX = 2_000_000_000L;
+    public static final Long PRICE_FOR_BONUS = 30_000_000L;
+    public static final Long PRICE_FOR_FIVE = 1_500_000L;
+    public static final Long PRICE_FOR_FOUR = 50_000L;
+    public static final Long PRICE_FOR_THREE = 5_000L;
     public static final Long PRICE_FOR_LESS_THAN_THREE = 0L;
+
+    public static final String INVALID_BONUS_NUMBER = "유효하지 않은 보너스 번호 입니다.";
+    public static final String INVALID_LOTTO_LENGTH = "로또는 6개 숫자여야 합니다.";
+    public static final String INVALID_LOTTO_UNIT = "로또 금액은 1,000원 단위여야 합니다.";
+    public static final String INVALID_NUMBER = "올바른 숫자를 입력해 주세요.";
+    public static final String INVALID_LOTTO_RANGE = "로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
 }
